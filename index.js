8f15d20f62f8afc129fe859a8fcf68cdf87447f7
iketerupanpani <ooohmikey@yahoo.co.jp>
2019-04-12 09:44:26 +0900

    フォーム送信とBody Parserの追加

--- index.js
+++ index.js
@@ -10,43 +10,31 @@ app.engine('ejs', ejs.renderFile);
 
 app.use(express.static('public'));
 
+var bodyParser = require('body-parser');
+
+//urlencodedの設定。送信されたフォームの内容が変換され取り出される。formを使うときに必ず使用する！
+app.use(bodyParser.urlencoded({ extended: false }));
 //ルーティンの設定（トップページにアクセスできるように）
 app.get('/', (req, res) => {
-    var msg = 'This is Express Page!<br>' + 'これは、スタイルシートを利用した例です。';
+    var msg = 'This is Index Page!<br>' + '※メッセージを書いて送信してください。';
     //index.ejsをレンダリングを実行する
     res.render('index.ejs', {
         title: 'Index',
         content: msg,
-        link: { href: '/other', text: '別のページに移動' },
-        link1: { href: '/other1', text: '別1のページに移動' }
-    });
-})
-
-//※otherページ
-app.get("/other", (req, res) => {
-    var msg = 'This is Other Page!<br>' + 'これは、用意された別のページです。';
-    res.render('index.ejs', {
-        title: 'other',
-        content: msg,
-        link: { href: '/', text: '※トップに戻る' },
-        link1: { href: '/other1', text: '別1のページに移動' }
-
     });
 });
 
-//※other1ページ
-app.get("/other1", (req, res) => {
-    var msg = 'This is Other1 Page!<br>' + 'これは、用意された別のページです。';
+//※POST送信の処理
+app.post('/', (req, res) => {
+    var msg = 'This is Posted Page!<br>' + 'あなたは「<b>' + req.body.message + '</b>」と送信しました。';
     res.render('index.ejs', {
-        title: 'other1',
+        title: 'Posted',
         content: msg,
-        link: { href: '/', text: '※トップに戻る' },
-        link1: { href: '/other', text: '別のページに移動' }
-
     });
 });
+
 
 //待ち受けの開始
-app.listen(3000, () => {
+var server = app.listen(3000, () => {
     console.log('Start is running!');
 })
